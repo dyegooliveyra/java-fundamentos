535f143a816871ba13b5f2c4a883678d2cfdd4a8
Diego Oliveira <diego.brito@harpio.com.br>
2025-02-22 22:08:58 -0300

    Repetition Structures

---
+++ src/repetitionStructures/RepetitionStructures.java
@@ -0,0 +1,25 @@
+package repetitionStructures;
+
+public class RepetitionStructures {
+
+    public static void main(String[] args) {
+    //while
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+    //do-while
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j < 5);
+
+    //for
+        for (int k = 0; k < 5; k++) {
+            System.out.println(k);
+        }
+    }
+}
